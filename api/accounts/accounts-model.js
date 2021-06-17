51e21630902ee76f1aaafb73dddb92cb930fa1f4
Stephen Aranda <aranda.stephen88@gmail.com>
2021-06-16 21:36:35 -0500

    functions in accounts-model have been completed

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,47 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('accounts').select('name','budget')
 }
 
-const getById = id => {
+ const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts')
+         .where('id',id)
+         .first()
 }
 
 const create = account => {
   // DO YOUR MAGIC
+  db('accounts').insert(account)
+  .then(([id])=>{
+    return getById(id)
+  })
+
 }
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  return db('accounts')
+         .where('id',id)
+         .update(account)
+         .then(()=>{
+           return getById(id)
+         })
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  let accountToBeRemoved
+  return getById(id)
+    .then(account => {
+      accountToBeRemoved = account
+      return db('accounts').where({ id }).del()
+    })
+    .then(numOfDeltedRecords => {
+      return accountToBeRemoved
+    })
 }
 
 module.exports = {
